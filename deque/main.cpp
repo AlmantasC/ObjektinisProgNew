0ca3793670925c201a9e9abb0126db30246f6eed
AlmantasC <almantas.cincikas@mif.stud.vu.lt>
2026-05-25 16:06:41 +0300

    Changed struct to class in deque

--- deque/main.cpp
+++ deque/main.cpp
@@ -23,10 +23,11 @@ int main(){
     SetConsoleCP(65001);
     std::srand(std::time(0));
     deque<studentas> A;
-    studentas temp;
     int m=0;
     auto ms = [](auto d){
-    return chr::duration<double, std::milli>(d).count();};
+        return chr::duration<double, std::milli>(d).count();
+    };
+
     // --------- PASIRINKIMAI ---------
 
     cout<<"[1] - Įvedimas ranka\n[2] - Generuoti tik pažymius\n[3] - Generuoti viską\n[4] - Skaitymas iš failo\n[5] - Failu generavimas\n[6] - Baigti darbą\nJūsų pasirinkimas: ";
@@ -59,31 +60,30 @@ int main(){
         case 4: skaitytiIsFailo(A, m, failas); break;
     }
     auto tNuskaitymasEnd = chr::high_resolution_clock::now();
-    cout<<"\nNuskaitymas:       "<< ms(tNuskaitymasEnd-tStart)    <<" ms";
+    cout<<"\nNuskaitymas:       "<< ms(tNuskaitymasEnd-tStart) <<" ms";
 
     // --------- SKAICIAVIMAI ---------
 
     switch (skaiciavimas){
-        case 1: for (auto& s : A) s.gal = 0.4*vid(s) + 0.6*s.egz; break;
-        case 2: for (auto& s : A) s.gal = 0.4*med(s) + 0.6*s.egz; break;
+        case 1: for (auto& s : A) s.setGal(0.4*s.vid() + 0.6*s.getEgz()); break;
+        case 2: for (auto& s : A) s.setGal(0.4*s.med() + 0.6*s.getEgz()); break;
     }
 
     // --------- SKIRSTYMAS ---------
-
     deque<studentas> nevykeliai;
     if (isvedimas==3) {
         auto tSkirstymasStart = chr::high_resolution_clock::now();
         skirstymas(A, nevykeliai);
         auto tSkirstymasEnd = chr::high_resolution_clock::now();
         cout<<"\nSkirstymas:        "<< ms(tSkirstymasEnd-tSkirstymasStart) <<" ms";
-        }
+    }
 
     // --------- RUSIAVIMAS ---------
     auto tRusiavimasStart = chr::high_resolution_clock::now();
     switch (rusiavimas) {
         case 1: sort(A.begin(), A.end(), pagalVard); if (isvedimas==3) sort(nevykeliai.begin(), nevykeliai.end(), pagalVard); break;
         case 2: sort(A.begin(), A.end(), pagalPavard); if (isvedimas==3) sort(nevykeliai.begin(), nevykeliai.end(), pagalPavard); break;
-        case 3: sort(A.begin(), A.end(), pagalGal); if (isvedimas==3)sort (nevykeliai.begin(), nevykeliai.end(), pagalGal); break;
+        case 3: sort(A.begin(), A.end(), pagalGal); if (isvedimas==3) sort(nevykeliai.begin(), nevykeliai.end(), pagalGal); break;
     }
     auto tRusiavimasEnd = chr::high_resolution_clock::now();
     cout<<"\nRūšiavimas:        "<< ms(tRusiavimasEnd-tRusiavimasStart) <<" ms";
@@ -108,8 +108,9 @@ int main(){
             std::ofstream foute("nerds.txt");
             printRez(foute, A, skaiciavimas);
             foute.close();
-            }
+            break;
         }
+    }
 
     auto tEnd = chr::high_resolution_clock::now();
     cout<<"\nIš viso užtruko:   "<< ms(tEnd-tStart) <<" ms\n";
